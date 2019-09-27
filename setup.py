229261e36e9edac4559cc9167360fbfde4c62b8f
Luke Carrier <luke@carrier.im>
2019-09-27 18:58:14 +0100

    Submit a description to PyPI

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import setup
+from os import path
+
+
+root_dir = path.abspath(path.dirname(__file__))
+with open(path.join(root_dir, 'README.md')) as f:
+    long_description = f.read()
 
 setup(
     name='mkdocs-drawio-exporter',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     version='0.3.0',
     packages=['mkdocsdrawioexporter'],
     url='https://github.com/LukeCarrier/mkdocs-drawio-exporter',
