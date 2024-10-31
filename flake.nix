ce8f79115736311f02a9da4f71bd5f3e533a087b
Luke Carrier <luke@carrier.family>
2024-10-31 18:50:16 +0000

    Package up a Flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "mkdocs-drawio-exporter";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { flake-utils, nixpkgs, ... }:
+  flake-utils.lib.eachDefaultSystem (system: {
+    devShells.default =
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+        pkgs.mkShell {
+          packages = with pkgs; [ python313 python312Packages.python-lsp-server ];
+        };
+  });
+}
