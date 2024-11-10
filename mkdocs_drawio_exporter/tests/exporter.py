LukeCarrier/mkdocs-drawio-exporter

commit f56f3d11eda60fb913945de6a2b38f2fea3e4c58

Rewrite embeds in on_page_markdown
Pre-process the Markdown before it's converted to Markdown, improving
compatibility with other plugins.

There are breaking changes here:

- In the `embed_format` configuration option:
  - `img_open` and `img_close` are no longer valid placeholders.
  - `img_alt` and `img_src` replace them.
  - The new default is `<img alt="{img_alt}" src="{img_src}">`.

Fixes #72

mkdocs_drawio_exporter/exporter.py
@@ -12,7 +12,7 @@ from typing import TypedDict
 import urllib.parse
 
 
-IMAGE_RE = re.compile('(<img[^>]+src=")([^">]+)("\\s*\\/?>)')
+IMAGE_RE = re.compile(r'!\[(?P<alt>[^\]]*)\]\((?P<filename>[^\)]*)\)')
 
 
 class Configuration(TypedDict):
@@ -262,12 +262,13 @@ class DrawIoExporter:
 
         def replace(match):
             try:
-                filename, page_index = match.group(2).rsplit('#', 1)
+                filename, page_index = match.group('filename').rsplit('#', 1)
             except ValueError:
-                filename = match.group(2)
+                filename = match.group('filename')
                 page_index = 0
+            img_alt = match.group('alt')
 
-            if fnmatch.fnmatch(filename, config["sources"]):
+            if fnmatch.fnmatch(filename, config['sources']):
                 source = Source(filename, page_index)
                 source.resolve_rel_path(page_dest_path)
                 content_sources.append(source)
@@ -276,7 +277,7 @@ class DrawIoExporter:
                 # Cache the file on-demand and read file content only if we
                 # need to inline the file's content.
                 content = None
-                if "{content}" in config["embed_format"]:
+                if '{content}' in config['embed_format']:
                     img_path = self.make_cache_filename(
                             source.source_rel, page_index, config['cache_dir'])
 
@@ -289,12 +290,11 @@ class DrawIoExporter:
                         self.log.error(f'Export failed with exit status {exit_status}; skipping rewrite')
                         return match.group(0)
 
-                    with open(img_path, "r") as f:
+                    with open(img_path, 'r') as f:
                         content = f.read()
 
-                return config["embed_format"].format(
-                        img_open=match.group(1), img_close=match.group(3),
-                        img_src=img_src, content=content)
+                return config['embed_format'].format(
+                        img_alt=img_alt, img_src=img_src, content=content)
             else:
                 return match.group(0)
         output_content = IMAGE_RE.sub(replace, output_content)

mkdocs_drawio_exporter/plugin.py
@@ -25,7 +25,7 @@ class DrawIoExporterPlugin(mkdocs.plugins.BasePlugin):
         ('drawio_executable', config_options.Type(str)),
         ('drawio_args', config_options.Type(list, default=[])),
         ('format', config_options.Type(str, default='svg')),
-        ('embed_format', config_options.Type(str, default='{img_open}{img_src}{img_close}')),
+        ('embed_format', config_options.Type(str, default='<img alt="{img_alt}" src="{img_src}">')),
         ('sources', config_options.Type(str, default='*.drawio')),
     )
 
@@ -53,9 +53,9 @@ class DrawIoExporterPlugin(mkdocs.plugins.BasePlugin):
                 f'arguments {self.config["drawio_args"]} and '
                 f'cache directory "{self.config["cache_dir"]}"')
 
-    def on_page_markdown(self, output_content, page, **kwargs):
+    def on_page_markdown(self, markdown, page, **kwargs):
         output_content, content_sources = self.exporter.rewrite_image_embeds(
-                page.file.dest_path, output_content, self.config)
+                page.file.dest_path, markdown, self.config)
 
         self.sources += content_sources
 

mkdocs_drawio_exporter/tests/exporter.py
@@ -34,7 +34,7 @@ class ExporterTests(unittest.TestCase):
             'drawio_executable': 'drawio',
             'drawio_args': [],
             'format': 'svg',
-            'embed_format': '{img_open}{img_src}{img_close}',
+            'embed_format': '<img alt="{img_alt}" src="{img_src}">',
             'sources': '*.drawio',
         }
         # FIXME: when dropping support for Python 3.8, replace with the merge
@@ -103,8 +103,9 @@ class ExporterTests(unittest.TestCase):
 
     def test_rewrite_image_embeds(self):
         page_dest_path = "index.html"
-        source = '''<h1>Example text</h1>
-<img alt="Some text" src="../some-diagram.drawio" />'''
+        source = '''# Example text
+
+![Some text](../some-diagram.drawio)'''
         object_embed_format = '<object type="image/svg+xml" data="{img_src}"></object>'
 
         exporter = self.make_exporter()

commit 750623058f94226be90d55e53d0fd5a43b6b8842

Fix source path resolution
Broke this in #75 🤦

mkdocs_drawio_exporter/exporter.py
@@ -126,14 +126,14 @@ class Source:
     def __repr__(self):
         return f"Source({self.source_embed}, {self.page_index}, {self.source_rel})"
 
-    def resolve_rel_path(self, page_dest_path):
+    def resolve_rel_path(self, page_src_path):
         """Resolve the path of the source, relative to the documentation directory.
 
-        :param str page_dest_path: The destination path of the parent page.
+        :param str page_src_path: The source path of the parent page.
         """
         unescaped_source_embed = urllib.parse.unquote(self.source_embed)
         self.source_rel = os.path.normpath(os.path.join(
-                os.path.dirname(page_dest_path),
+                os.path.dirname(page_src_path),
                 unescaped_source_embed))
 
 
@@ -248,7 +248,7 @@ class DrawIoExporter:
                     'embed_format', config['embed_format'],
                     'cannot inline content of non-SVG format')
 
-    def rewrite_image_embeds(self, page_dest_path, output_content, config: Configuration):
+    def rewrite_image_embeds(self, page_src_path, output_content, config: Configuration):
         """Rewrite image embeds.
 
         :param str page_dest_path: Destination path.
@@ -270,7 +270,7 @@ class DrawIoExporter:
 
             if fnmatch.fnmatch(filename, config['sources']):
                 source = Source(filename, page_index)
-                source.resolve_rel_path(page_dest_path)
+                source.resolve_rel_path(page_src_path)
                 content_sources.append(source)
                 img_src = f"{filename}-{page_index}.{config['format']}"
 

mkdocs_drawio_exporter/plugin.py
@@ -55,7 +55,7 @@ class DrawIoExporterPlugin(mkdocs.plugins.BasePlugin):
 
     def on_page_markdown(self, markdown, page, **kwargs):
         output_content, content_sources = self.exporter.rewrite_image_embeds(
-                page.file.dest_path, markdown, self.config)
+                page.file.src_path, markdown, self.config)
 
         self.sources += content_sources
 

mkdocs_drawio_exporter/tests/exporter.py
@@ -5,7 +5,7 @@ import logging
 import os
 from os.path import join, sep
 
-from ..exporter import Configuration, ConfigurationError, DrawIoExporter
+from ..exporter import Configuration, ConfigurationError, DrawIoExporter, Source
 
 
 class FileMock:
@@ -17,12 +17,25 @@ class FileMock:
             setattr(self, attr, value)
 
 
+class SourceTests(unittest.TestCase):
+    def test_resolve_rel_path(self):
+        cases = [
+            ("dir/diagram.drawio", ("diagram.drawio", 0), "dir/page.md"),
+            ("dir1/dir2/diagram.drawio", ("diagram.drawio", 0), "dir1/dir2/index.md"),
+            ("dir1/dir2/dir3/diagram.drawio", ("diagram.drawio", 0), "dir1/dir2/dir3/index.md"),
+        ]
+        for expect, source_args, page_src_path in cases:
+            with self.subTest(expect, source_args, page_src_path):
+                source = Source(*source_args)
+                result = source.resolve_rel_path(page_src_path)
+                self.assertEqual(expect, result)
+
+
 class ExporterTests(unittest.TestCase):
     log = None
 
     def setUp(self):
         self.log = logging.getLogger(__name__)
-
     def make_exporter(self, docs_dir=None):
         if not docs_dir:
             docs_dir = sep + 'docs'